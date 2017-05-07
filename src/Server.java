7b4a149c14c7b16190346e5f672eead247cea1b1
paarthmadan <madanhockey@hotmail.com>
2017-05-06 20:20:18 -0400

    (wip): server

---
+++ src/Server.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+	
+	private ServerSocket server = null;
+	private Socket socket = null;
+	private DataInputStream streamInput = null;
+	
+	public Server(int portNumber){
+	
+		try{
+			System.out.println("Creating server on port " + portNumber + ", please wait...");
+			server = new ServerSocket(portNumber);
+			System.out.println("Server created: " + server);
+			
+			System.out.println("Waiting for client...");
+			socket = server.accept();
+			
+			//open socket and streams
+			open();
+			
+			boolean isClientDone = false;
+			
+			while(streamInput.readUTF() != null && !isClientDone){
+				
+				String input = streamInput.readUTF();
+				System.out.println(input);
+				isClientDone = input.equalsIgnoreCase("done");
+				
+			}
+			
+			//close socket and streams
+			close();
+			
+		}catch(IOException e){
+			System.out.println(e);
+		}
+	}
+	
+	public void open() throws IOException{
+		streamInput = new DataInputStream(socket.getInputStream());
+	}
+	
+	public void close() throws IOException{
+		socket.close();
+		streamInput.close();
+	}
+	
+}
